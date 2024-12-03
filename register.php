1de576413dbce0a5213bf497fda9cbef7e4b9357
cele-bug <jordi.celemin@gmail.com>
2024-12-03 10:23:52 +0100

    Cambios en el login y register
    Cambios en los botones y flecha

--- register.php
+++ register.php
@@ -5,6 +5,7 @@ require 'php/insertRegister.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,46 +15,45 @@ require 'php/insertRegister.php';
     <link rel="stylesheet" href="style.css">
     <title>Registro</title>
 </head>
+
 <body>
-<div class="bodycss">
-    <nav class="navLogin">
-        <button type="button" id="back-button"><img src="./LangingPagPhoto/flecha.png" alt="" class="flechaVolver"></button>
+    <div class="bodycss">
+        <nav class="navLogin">
+            <button type="button" id="back-button"><img src="./LangingPagPhoto/flecha.png" alt=""
+                    class="flechaVolver"></button>
 
-        <div class="idioma">
-                    <img src="LangingPagPhoto/spain.png" alt="es">
-                    <img src="LangingPagPhoto/catalan.png" alt="ca">
-                    <img src="LangingPagPhoto/english.png" alt="en">
-        </div>
-    </nav>
+            <div class="cambioIdioma">
+                <button id="lang-es"><img src="./LangingPagPhoto/mundo.png" alt=""></button>
+                <button id="lang-ca"><img src="./LangingPagPhoto/bandera.png" alt=""></button>
+                <button id="lang-en"><img src="./LangingPagPhoto/reino-unido.png" alt=""></button>
+            </div>
+        </nav>
 
-    <h1 id="register-title">REGISTRARSE</h1>
-    <?php include 'php/mensaje.php'; ?>
-    <div class="container">
-        <div class="capa1"></div>
-        <div class="login-container-register">
-       
-            <form method="POST" action="">
-                <label id="name-label" for="name">Nombre</label>
-                <br>
-                <input type="name" name="username" id="name" required>
-                
-                <br>
+        <h1 id="register-title">REGISTRARSE</h1>
+        <?php include 'php/mensaje.php'; ?>
+        <div class="container">
+            <div class="capa1"></div>
+            <div class="login-container-register">
 
-                <label id="email-label" for="email" >Correo electrónico</label>
-                <br>
-                <input type="email" id="email" name="email"  required>
-                
-                <br>
-                
-                <label id="password-label" for="password" >Contraseña</label>
-                <br>
-                <input type="password" id="password"  name="password" required>
-                <button type="submit" id="register-button">REGISTRARSE</button>
-           
-            </form>
+                <form method="POST" action="">
+                    <label id="name-label" for="name">Nombre</label>
+                    <br>
+                    <input type="name" name="username" id="name" required>
+                    <br>
+                    <label id="email-label" for="email">Correo electrónico</label>
+                    <br>
+                    <input type="email" id="email" name="email" required>
+                    <br>
+                    <label id="password-label" for="password">Contraseña</label>
+                    <br>
+                    <input type="password" id="password" name="password" required>
+                    <button type="submit" id="register-button">REGISTRARSE</button>
+
+                </form>
+            </div>
         </div>
     </div>
-</div>
-<script src="main.js"></script>
+    <script src="main.js"></script>
 </body>
+
 </html>
